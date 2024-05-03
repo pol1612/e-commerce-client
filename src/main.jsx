a787636fb411ab0abd64eb73db106755c5012c80
Pol Sañé Jové <pol.sane@uvic.cat>
2024-05-03 18:47:13 +0100

    added first route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
-
+import {HashRouter as Router, Routes, Route} from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route index element={<App />} />
+            </Routes>
+        </Router>
+    </React.StrictMode>
 )
