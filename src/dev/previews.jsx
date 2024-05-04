0c9b46e6debb152a3bd4feb1b369d2817f95337d
Pol Sañé Jové <pol.sane@uvic.cat>
2024-05-04 09:40:52 +0100

    changed  .gitignore

--- src/dev/previews.jsx
+++ src/dev/previews.jsx
@@ -1,9 +1,20 @@
-import {Previews} from '@react-buddy/ide-toolbox'
+import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox'
 import {PaletteTree} from './palette'
+import HomePage from "../pages/HomePage.jsx";
+import ProductCard from "../components/ProductCard.jsx";
 
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree/>}>
+            <ComponentPreview path="/HomePage">
+                <HomePage/>
+            </ComponentPreview>
+            <ComponentPreview path="/ProductCard">
+                <ProductCard/>
+            </ComponentPreview>
+            <ComponentPreview path="/ComponentPreviews">
+                <ComponentPreviews/>
+            </ComponentPreview>
         </Previews>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {HashRouter as Router, Routes, Route} from 'react-router-dom'
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev/index.js";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Router>
             <Routes>
-                <Route index element={<App />} />
+                <Route index element={<DevSupport ComponentPreviews={ComponentPreviews}
+                                                  useInitialHook={useInitial}
+                >
+                    <App/>
+                </DevSupport>}/>
             </Routes>
         </Router>
     </React.StrictMode>
